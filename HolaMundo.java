cc61c0f97d156d15f82e2ab7cdd87a85184b05d2
Juan Bodelo <jrbodelo@gmail.com>
2018-04-10 22:20:46 +0200

    Creando una versión mejorada

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,8 +2,9 @@
 * Muestra por pantalla la frase "Hola mundo"
 * @author Juan Bodelo
 */
-class Holamundo{
+class HolaMundo{
 	public static void main(String[] args){
 		System.out.println("Hola Mundo");
+		System.out.println("Aquí estamos haciendo pruebas con Git");
 	}
 }
