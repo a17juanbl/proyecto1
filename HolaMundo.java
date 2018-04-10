3230c0afbce6803ed0e9fc411121a84a3f342928
Juan Bodelo <jrbodelo@gmail.com>
2018-04-10 19:23:10 +0200

    comentarios añadidos al código

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,3 +1,8 @@
+/* 
+* Muestra por pantalla la frase "Hola mundo"
+* @author Juan Bodelo
+*/
+
 public class Holamundo{
 	public static void main(String[] args){
 		System.out.println("Hola Mundo");
